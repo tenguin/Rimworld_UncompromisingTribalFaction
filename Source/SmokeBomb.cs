fb99570a0eabe7b5a3be1e1e48e7911c08982fe1
tenguin <73805320+tenguin@users.noreply.github.com>
2021-08-10 17:14:51 -0400

    Initial upload

---
+++ Source/SmokeBomb.cs
@@ -0,0 +1,27 @@
+﻿using RimWorld;
+using Verse;
+
+namespace UncompromisingTribalFaction
+{
+	public class SmokeBomb : Apparel
+	{
+		private float ApparelScorePerBeltRadius = 0.046f;
+		public override void Notify_BulletImpactNearby(BulletImpactData impactData)
+		{
+			base.Notify_BulletImpactNearby(impactData);
+			Pawn wearer = base.Wearer;
+			if (wearer != null && !wearer.Dead && !impactData.bullet.def.projectile.damageDef.isExplosive && impactData.bullet.Launcher != null && impactData.bullet.Launcher.HostileTo(base.Wearer) && !wearer.IsColonist && wearer.Spawned)
+			{
+				Gas gas = wearer.Position.GetGas(wearer.Map);
+				if (gas == null || !gas.def.gas.blockTurretTracking)
+				{
+					Verb_SmokePop.Pop(this.TryGetComp<CompReloadable>());
+				}
+			}
+		}
+		public override float GetSpecialApparelScoreOffset()
+		{
+			return this.GetStatValue(StatDefOf.SmokepopBeltRadius) * ApparelScorePerBeltRadius;
+		}
+	}
+}
