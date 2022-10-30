2af35a7f71b2b40310cb2381b64bafe6cc082d24
tenguin <73805320+tenguin@users.noreply.github.com>
2022-10-30 00:18:39 -0400

    2.0 for rimworld 1.4

---
+++ Source/FilthDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace UncompromisingTribalFaction
+{
+	[DefOf]
+	public static class FilthDefOf
+	{
+		public static ThingDef UncompromisingTribalFaction_Filth_FireArrow;
+
+		static FilthDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(FilthDefOf));
+		}
+	}
+}
---
+++ Source/FuuFireArrow.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+
+namespace UncompromisingTribalFaction
+{
+    public class FuuFireArrow : Projectile
+	{
+		private static FloatRange FireSizeRange = new FloatRange(0.5f, 0.8f);
+
+		protected override void Impact(Thing hitThing, bool blockedByShield = false)
+		{
+			if (!base.Position.GetTerrain(base.Map).IsWater)
+			{
+				FilthMaker.TryMakeFilth(base.Position, base.Map, FilthDefOf.UncompromisingTribalFaction_Filth_FireArrow, 1, FilthSourceFlags.None, false);
+				FireUtility.TryStartFireIn(base.Position, base.Map, FireSizeRange.RandomInRange);
+			}
+			FleckCreationData dataStatic = FleckMaker.GetDataStatic(DrawPos, base.Map, FleckDefOf.MicroSparksFast);
+			dataStatic.velocitySpeed = 0.8f;
+			dataStatic.velocityAngle = ExactRotation.eulerAngles.y;
+			base.Map.flecks.CreateFleck(dataStatic);
+			base.Impact(hitThing, blockedByShield);
+		}
+	}
+}
