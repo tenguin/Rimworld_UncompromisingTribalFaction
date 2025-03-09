b010316955de5f657cfe5269bafaa3282a3fe4bc
tenguin <73805320+tenguin@users.noreply.github.com>
2025-03-09 18:15:34 -0400

    Update for 1.5

--- Source/SmokeBomb.cs
+++ Source/SmokeBomb.cs
@@ -3,6 +3,8 @@ using Verse;
 
 namespace UncompromisingTribalFaction
 {
+	//Mostly duplicated from Rimworld.SmokepopBelt
+	//However, only triggers on turret fire requirement is removed, so it will now trigger on any source of firearms fire.
 	public class SmokeBomb : Apparel
 	{
 		private float ApparelScorePerBeltRadius = 0.046f;
@@ -12,14 +14,14 @@ namespace UncompromisingTribalFaction
 			Pawn wearer = base.Wearer;
 			if (wearer != null && !wearer.Dead && !impactData.bullet.def.projectile.damageDef.isExplosive && impactData.bullet.Launcher != null &&
 				impactData.bullet.Launcher.HostileTo(base.Wearer) && !wearer.IsColonist && wearer.Spawned && !wearer.Position.AnyGas(wearer.Map, GasType.BlindSmoke))
-			{
-					Verb_SmokePop.Pop(this.TryGetComp<CompReloadable>());
+            {
+                Verb_SmokePop.Pop(this.TryGetComp<CompApparelReloadable>());
 			}
 		}
 
 		public override float GetSpecialApparelScoreOffset()
-		{
-			return this.GetStatValue(StatDefOf.SmokepopBeltRadius) * ApparelScorePerBeltRadius;
+        {
+            return this.GetStatValue(StatDefOf.PackRadius) * ApparelScorePerBeltRadius;
 		}
 	}
 }
